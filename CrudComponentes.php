512e15a46c0a5b6ec7903144ab33484b3a6e50c8
Everton Otavio Afonso <evertonafonso0@gmail.com>
2020-05-05 08:13:17 -0300

    add função logout

--- CrudComponentes.php
+++ CrudComponentes.php
@@ -5,33 +5,40 @@
     Class Componentes{
 
       //verifica se o usuário efetuou o login com sucesso 
-      public function login($user, $pass){
-          $pdo = conexao();
+        public function login($user, $pass){
+            $pdo = conexao();
 
-          $select = $pdo->prepare("SELECT pass FROM usuario");
-          $select->execute();
-          $result = $select->fetch(PDO::FETCH_ASSOC);
+            $select = $pdo->prepare("SELECT pass FROM usuario");
+            $select->execute();
+            $result = $select->fetch(PDO::FETCH_ASSOC);
 
-          foreach ($result as $key => $value){
-              if(password_verify($pass, $value)){
-                  $dados = $pdo->prepare("SELECT * FROM usuario WHERE user = :user AND pass = :pass");
-                  $dados->bindValue("user", $user);
-                  $dados->bindValue("pass", $value);
-                  $dados->execute();
-      
-                  if($dados->rowCount() > 0){
-                      $id = $dados->fetch();
-          
-                      $_SESSION['id'] = $id['idusuario'];
-                      return true;
-                  }else{
-                      return flase;
-                  }        
-              }else{
-                  echo "Erro";
-              }
-          }
-      }
+            foreach ($result as $key => $value){
+                if(password_verify($pass, $value)){
+                    $dados = $pdo->prepare("SELECT * FROM usuario WHERE user = :user AND pass = :pass");
+                    $dados->bindValue("user", $user);
+                    $dados->bindValue("pass", $value);
+                    $dados->execute();
+        
+                    if($dados->rowCount() > 0){
+                        $id = $dados->fetch();
+            
+                        $_SESSION['id'] = $id['idusuario'];
+                        return true;
+                    }else{
+                        return flase;
+                    }        
+                }else{
+                    echo "Erro";
+                }
+            }
+        }
+
+      // exit destrói a seção do usuário
+        public function exit(){
+            session_start();
+            session_destroy();
+            header('location: index.php');
+        }
     }
 
 ?>
---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+    // Faz o usuario sair da aplicação
+    require_once 'CrudComponentes.php';
+    $componentes = new Componentes();
+    $componentes->exit();
+
+?>
