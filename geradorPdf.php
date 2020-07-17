19a463f14e2522788aa396084130c18dc8bbdcdc
Emanuel-tron <emanueleite11@gmail.com>
2020-07-16 23:28:21 -0300

    add geradorPdf.php

---
+++ geradorPdf.php
@@ -0,0 +1,22 @@
+<?php
+
+    use Dompdf\Dompdf;
+
+    // include autoloader
+    require_once './dompdf/autoload.inc.php';
+
+    $dompdf = new DOMPDF();
+
+    ob_start();
+    require_once 'telaExemploPdf.php';
+    $dompdf->loadHtml(ob_get_clean());
+    
+    $dompdf->setPaper("A4");
+
+    $dompdf->render();
+    $dompdf->stream(
+        "Relatorio de componentes cadastrados.pdf",
+        ["Attachment" => false]
+    );
+
+?>
