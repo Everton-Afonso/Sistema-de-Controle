8d7fd1b215990d95c36b784eff2383ee31fd60a7
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-03-04 10:34:07 -0300

    alteração da pagina de verificação do sistema

--- verifica.php
+++ verifica.php
@@ -1,11 +1,12 @@
 <?php
 
+session_start(); 
     require_once './conexao/conexao.php';
     $pdo = conexao();
 
     if (isset($_SESSION['id']) && !empty($_SESSION['id'])) {
-        require_once 'classes/CrudComponentes.class.php';
-        $idUser = new Componente();
+        require_once './classes/CrudEstoque.class.php';
+        $idUser = new Estoque();
         $list = $idUser->logado($_SESSION['id']);
 
     } else {
